66d0e3550f2e3d4e117156c23bf694c3ba8c58ba
Artur Talimonchyk <artur.talimonchyk@gmail.com>
2021-07-01 15:13:12 +0300

    input data

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,9 +6,15 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            string s = "laxaban";
-            Console.WriteLine(s);
-            
+            /*string s = "laxaban";
+             Console.WriteLine(s);*/
+
+            string data;
+
+            data = "Name " + Console.ReadLine();
+
+            Console.WriteLine(data + "some");
+            Console.WriteLine(data);
         }
     }
 }
