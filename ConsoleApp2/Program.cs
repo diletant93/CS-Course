f6671f70916a92b00576d55458fedc095416f011
Artur Talimonchyk <artur.talimonchyk@gmail.com>
2021-07-02 11:44:17 +0300

    Converter

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace ConsoleApp2
 {
     class Program
@@ -7,14 +6,29 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             /*string s = "laxaban";
-             Console.WriteLine(s);*/
-
+             Console.WriteLine(s);
+            ///////////////////////
             string data;
 
-            data = "Name " + Console.ReadLine();
+            data = Console.ReadLine();
 
             Console.WriteLine(data + "some");
             Console.WriteLine(data);
+            //////////////////////
+            */
+
+            /*string str;
+            Console.WriteLine("Write 1 num:");
+            str = Console.ReadLine();
+            int num1 = Convert.ToInt16(str);
+            Console.WriteLine("Write 2 num:");
+            str = Console.ReadLine();
+            int num2 = Convert.ToInt16(str);
+            Console.WriteLine("Sum = " + (num1 + num2));
+            */
+            string str = "12,321";
+            double b = Convert.ToDouble(str);
+            Console.WriteLine(b);
         }
     }
 }
