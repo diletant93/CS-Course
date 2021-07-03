fee897192efdc3d7fdbf346633161900d9cef797
Artur Talimonchyk <artur.talimonchyk@gmail.com>
2021-07-03 12:10:24 +0300

    calculator

---
+++ homeworks/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //switch
+            //Console.WriteLine("Enter num1: ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter num2: ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Choose the operation: + - / * :");
+            //string operation = Console.ReadLine();
+            //if(b == 0 && operation == "/")
+            //{
+            //    Console.WriteLine("You can't divide by 0!");
+            //}
+            //else
+            //{
+            //    switch (operation)
+            //    {
+            //        case "+":
+            //            Console.WriteLine("num1 + num2 = " + (a + b));
+            //            break;
+            //        case "-":
+            //            Console.WriteLine("num1 - num2 = " + (a - b));
+            //            break;
+            //        case "*":
+            //            Console.WriteLine("num1 * num2 = " + (a * b));
+            //            break;
+            //        case "/":
+            //            Console.WriteLine("num1 / num2 = " + (a / b));
+            //            break;
+            //        default:
+            //            Console.WriteLine("Error.");
+            //            break;
+            //    }
+            //}
+            //if
+            Console.WriteLine("Enter num1: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter num2: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Choose the operation: + - / * :");
+            string operation = Console.ReadLine();
+            if (b == 0 && operation == "/")
+            {
+                Console.WriteLine("You can't divide by 0!");
+            }
+            else
+            {
+                if(operation == "+")
+                {
+                    Console.WriteLine("num1 + num2 = " + (a + b));
+                }
+                if (operation == "-")
+                {
+                    Console.WriteLine("num1 - num2 = " + (a - b));
+                }
+                if (operation == "*")
+                {
+                    Console.WriteLine("num1 * num2 = " + (a * b));
+                }
+                if (operation == "/")
+                {
+                    Console.WriteLine("num1 / num2 = " + (a / b));
+                }
+
+            }
+        }
+    }
+}
