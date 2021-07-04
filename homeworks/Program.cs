743c108aeb76b232275da15212eea2cd34977c76
Artur Talimonchyk <artur.talimonchyk@gmail.com>
2021-07-04 12:13:19 +0300

    triangles homework

--- homeworks/Program.cs
+++ homeworks/Program.cs
@@ -76,41 +76,94 @@ namespace homeworks
 
 
             //while number of even and odd in current borders
-            Console.WriteLine("Enter first border: ");
-            int border1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter last border: ");
-            int border2 = Convert.ToInt32(Console.ReadLine());
-            if (border1 > border2)
-            {
-                int tmp = border2;
-                border2 = border1;
-                border1 = tmp;
-            }
+            //Console.WriteLine("Enter first border: ");
+            //int border1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter last border: ");
+            //int border2 = Convert.ToInt32(Console.ReadLine());
+            //if (border1 > border2)
+            //{
+            //    int tmp = border2;
+            //    border2 = border1;
+            //    border1 = tmp;
+            //}
 
-            int countEven = 0;
-            int SumEven = 0;
+            //int countEven = 0;
+            //int SumEven = 0;
 
-            int countOdd = 0;
-            int SumOdd = 0;
-            for (int i = border1; i <= border2; i++)
+            //int countOdd = 0;
+            //int SumOdd = 0;
+            //for (int i = border1; i <= border2; i++)
+            //{
+            //    if (border1 % 2 == 0)
+            //    {
+            //        countEven++;
+            //        SumEven += border1;
+            //    }
+            //    else
+            //    {
+            //        countOdd++;
+            //        SumOdd += border1;
+            //    }
+            //    border1++;
+            //}
+
+            //Console.WriteLine("Number of Even = " + countEven);
+            //Console.WriteLine("Sum of Even = " + SumEven);
+            //Console.WriteLine("Number of Odd = " + countOdd);
+            //Console.WriteLine("Sum of Odd = " + SumOdd);
+            //ulong i = ulong.Parse(Console.ReadLine());
+            //for (; i > 0; i--)
+            //{
+            //    Console.WriteLine("i = " + i );
+            //}
+            //*
+            //**
+            //***
+            //****
+            //*****
+            //****** width = 6 and height   = 6
+            //break
+            Console.Write("Enter height:");
+            int height = int.Parse(Console.ReadLine());
+            int tmp3 = height;
+            for (int i = 0; i < height; i++)
             {
-                if (border1 % 2 == 0)
+                for (int j = --tmp3; j < height; j++)
                 {
-                    countEven++;
-                    SumEven += border1;
+                    Console.Write("#");
                 }
-                else
-                {
-                    countOdd++;
-                    SumOdd += border1;
-                }
-                border1++;
+                Console.WriteLine();
             }
-            
-            Console.WriteLine("Number of Even = " + countEven);
-            Console.WriteLine("Sum of Even = " + SumEven);
-            Console.WriteLine("Number of Odd = " + countOdd);
-            Console.WriteLine("Sum of Odd = " + SumOdd);
+            Console.Write("Enter height:");
+            int height2 = int.Parse(Console.ReadLine());
+            int tmp = height2/*10*/ , tmp2 = height2/*10*/;
+            for (int i = 0; i < height2; i++)
+            {
+                tmp--;
+                for (int j = 0; j < tmp; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int m = --tmp2; m < height2; m++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            //
+            //           *
+            //          **
+            //         ***
+            //        ****
+            //       *****
+            //      ****** 
+            //     *******
+            //    ********
+            //   *********
+            //  **********
+            // ***********
+            //************
+            //HEIGHT 12
         }
     }
 }
