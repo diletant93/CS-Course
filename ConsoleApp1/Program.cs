c2aa74994436d4d4107188c9cb2226bdb8f22c6e
Artur Talimonchyk <artur.talimonchyk@gmail.com>
2021-07-07 10:33:32 +0300

    work with arrays

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,14 @@ namespace ConsoleApp1
 {
     class Program
     {
+        //static int Sum(int a , int b)
+        //{
+        //    return a + b;
+        //}
+        static void PrintLine()
+        {
+            Console.WriteLine("The functiong is working!");
+        }
         static void Main(string[] args)
         {
             Random random = new Random();
@@ -97,38 +105,40 @@ namespace ConsoleApp1
             //    }
             //    Console.WriteLine("#################");
             //}
-            int[][][] array = new int[random.Next(2,10)][][];
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = new int[random.Next(2, 10)][];
-                for (int j = 0; j < array[i].Length; j++)
-                {
-                    array[i][j] = new int[random.Next(2, 10)];
-                    for (int k = 0; k < array[i][j].Length; k++)
-                    {
-                        array[i][j][k] = random.Next(-100, 100);
-                    }
-                }
-            }
-            for (int i = 0; i < array.Length; i++)
-            {
-            
-                for (int j = 0; j < array[i].Length; j++)
-                {
-                    
-                    for (int k = 0; k < array[i][j].Length; k++)
-                    {
-                        Console.Write(array[i][j][k] + "\t");
-                    }
-                    Console.WriteLine();
-                    Console.WriteLine();
-                }
-                Console.WriteLine();
-                Console.WriteLine("######################");
-                Console.WriteLine();
-            }
+            //int[][][] array = new int[random.Next(2,10)][][];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = new int[random.Next(2, 10)][];
+            //    for (int j = 0; j < array[i].Length; j++)
+            //    {
+            //        array[i][j] = new int[random.Next(2, 10)];
+            //        for (int k = 0; k < array[i][j].Length; k++)
+            //        {
+            //            array[i][j][k] = random.Next(-100, 100);
+            //        }
+            //    }
+            //}
+            //for (int i = 0; i < array.Length; i++)
+            //{
 
+            //    for (int j = 0; j < array[i].Length; j++)
+            //    {
 
+            //        for (int k = 0; k < array[i][j].Length; k++)
+            //        {
+            //            Console.Write(array[i][j][k] + "\t");
+            //        }
+            //        Console.WriteLine();
+            //        Console.WriteLine();
+            //    }
+            //    Console.WriteLine();
+            //    Console.WriteLine("######################");
+            //    Console.WriteLine();
+            //}
+            //Функції і методи
+            //int result = Sum(random.Next(-100,100), random.Next(-100, 100));
+            //Console.WriteLine(result); 
+            PrintLine();
         }
     }
 }
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void PrintString(char symbol, int times)
+        {
+            string str = "";
+            for (int i = 0; i < times; i++)
+            {
+                str += symbol;
+            }
+            Console.WriteLine("Str = " + str);
+        }
+        static int IndexOf(int[] array , int number)
+        {
+            int index = -1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(number == array[i])
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        }
+        static void Main(string[] args)
+        {
+            PrintString(Convert.ToChar(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            int[] arr = { 1, 23, 412423, 43,123, -21312, 43, -54 };
+            Console.WriteLine("Index = " + IndexOf(arr, 100));
+        }
+    }
+}
