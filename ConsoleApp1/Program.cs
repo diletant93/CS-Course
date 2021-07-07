c88404741f0b300b1e5d3dcad04595cbf44d7722
Artur Talimonchyk <artur.talimonchyk@gmail.com>
2021-07-07 09:40:07 +0300

    three dimension array

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-
+using System.Linq;
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
             //Console.WriteLine("Hello World!");
             //int[] array = { 1, 2, 32, 3, 23, 3 };
             //Console.WriteLine(array[^2]);
@@ -29,24 +30,104 @@ namespace ConsoleApp1
             //{
             //    Console.WriteLine($"item = {item}");
             //}
-            int[,] array = new int[int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())];
-            Random random = new Random();
-            for (int i = 0; i < array.GetLength(0); i++)
+            //int[,] array = new int[int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())];
+            //Random random = new Random();
+            //for (int i = 0; i < array.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < array.GetLength(1); j++)
+            //    {
+            //        array[i, j] = random.Next(-100,100);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int i = 0; i < array.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < array.GetLength(1); j++)
+            //    {
+            //        Console.Write(array[i, j] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.Write("Enter lenght of array of arrays:");
+            //int[][] array = new int [int.Parse(Console.ReadLine())][];
+            //Random random = new Random();
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"Enter lenght of {(i+1)} array:");
+            //    array[i] = new int[int.Parse(Console.ReadLine())];
+            //}
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    for (int j = 0; j < array[i].Length; j++)
+            //    {
+            //        array[i][j] = random.Next(-100, 100);
+            //    }
+            //}
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    for (int j = 0; j < array[i].Length; j++)
+            //    {
+            //        Console.Write(array[i][j] + " \t");
+            //    }
+            //    Console.WriteLine();
+            //    Console.WriteLine();
+            //}
+            //Тривимірний масив, багатовимірні масиви
+            //int[,,] array = new int[6,4, 5];
+            //Random random = new Random();
+            //for (int i = 0; i < array.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < array.GetLength(1); j++)
+            //    {
+            //        for (int k = 0; k < array.GetLength(2); k++)
+            //        {
+            //            array[i, j, k] = random.Next(-100, 100);
+            //        }
+            //    }
+            //}
+            //for (int i = 0; i < array.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < array.GetLength(1); j++)
+            //    {
+            //        for (int k = 0; k < array.GetLength(2); k++)
+            //        {
+            //            Console.Write(array[i, j, k] + "\t");
+            //        }
+            //        Console.WriteLine();
+            //    }
+            //    Console.WriteLine("#################");
+            //}
+            int[][][] array = new int[random.Next(2,10)][][];
+            for (int i = 0; i < array.Length; i++)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
+                array[i] = new int[random.Next(2, 10)][];
+                for (int j = 0; j < array[i].Length; j++)
                 {
-                    array[i, j] = random.Next(-100,100);
+                    array[i][j] = new int[random.Next(2, 10)];
+                    for (int k = 0; k < array[i][j].Length; k++)
+                    {
+                        array[i][j][k] = random.Next(-100, 100);
+                    }
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+            
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    
+                    for (int k = 0; k < array[i][j].Length; k++)
+                    {
+                        Console.Write(array[i][j][k] + "\t");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
                 }
                 Console.WriteLine();
-            }
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write(array[i, j] + "\t");
-                }
+                Console.WriteLine("######################");
                 Console.WriteLine();
             }
+
 
         }
     }
